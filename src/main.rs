725f235856966a188f30052e6a0b711268d50a55
succcubbus <jannis.kaiser2@gmail.com>
2019-04-21 15:58:53 +0200

    allow specifying custom delimiter, improve line processing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use clap::{App, Arg};
 use regex::Regex;
+use std::borrow::ToOwned;
 use std::io::{self, BufRead};
 use std::ops::Range;
+use std::result::Result;
 
 #[derive(Debug)]
 enum CutsError {
     NonIntegerSelection(String),
     MalformedSelection(String),
-    InputClosedUnexpectedly,
+    InvalidDelimiter(String),
 }
 
 fn main() -> Result<(), CutsError> {
@@ -21,52 +23,79 @@ fn main() -> Result<(), CutsError> {
                 .required(true)
                 .index(1),
         )
+        .arg(
+            Arg::with_name("string_delimiter")
+                .short("d")
+                .takes_value(true)
+                .help("use this delimiter instead of whitespace")
+                .group("delimiter"),
+        )
+        .arg(
+            Arg::with_name("regex_delimiter")
+                .short("D")
+                .takes_value(true)
+                .help("use this regex as the delimiter instead of whitespace")
+                .group("delimiter"),
+        )
         .get_matches();
 
     let selections = matches.value_of("SELECTION").unwrap();
     let selections = parse_selection(selections)?;
     println!("parsed selections: {:?}", selections);
 
-    let separator = Regex::new(r"\s+").unwrap();
+    let separator = matches
+        .value_of("regex_delimiter")
+        .map(ToOwned::to_owned)
+        .or_else(|| matches.value_of("string_delimiter").map(regex::escape))
+        .unwrap_or_else(|| r"\s+".to_owned());
+    let split_regex = Regex::new(&separator).map_err(|_| CutsError::InvalidDelimiter(separator))?;
 
-    let stdin = io::stdin();
-    let mut handle = stdin.lock();
+    io::stdin()
+        .lock()
+        .lines()
+        .filter_map(Result::ok) // filter out lines with invalud utf-8
+        .for_each(|line| {
+            let fields = &split_regex.split(&line).collect::<Vec<_>>();
 
-    loop {
-        let mut input = String::new();
-        handle
-            .read_line(&mut input)
-            .map_err(|_| CutsError::InputClosedUnexpectedly)?;
+            let output = selections
+                .iter()
+                .map(|selection| to_concrete_range(selection, fields.len()))
+                .flat_map(|range| fields[range].to_vec())
+                .collect::<Vec<&str>>()
+                .join(" ");
 
-        let fields = &separator.split(&input).collect::<Vec<_>>();
+            println!("{}", output);
+        });
 
-        let output = selections
-            .iter()
-            .flat_map(|selection| match selection {
-                Selection::Single(column) => vec![fields[to_concrete_index(*column, fields.len())]],
-                Selection::Range(start, end) => {
-                    let start = start
-                        .map(|index| to_concrete_index(index, fields.len()))
-                        .unwrap_or(0);
-                    let end = end
-                        .map(|index| to_concrete_index(index, fields.len()))
-                        .unwrap_or_else(|| fields.len());
-                    let concrete_range = Range { start, end };
-                    fields[concrete_range].to_vec()
-                }
-            })
-            .collect::<Vec<&str>>()
-            .join(" ");
+    Ok(())
+}
 
-        println!("{}", output);
+fn to_concrete_range(selection: &Selection, num_fields: usize) -> Range<usize> {
+    #[allow(clippy::range_plus_one)]
+    match selection {
+        Selection::Single(column) => to_concrete_index(*column, num_fields)
+            .map(|index| index..index + 1)
+            .unwrap_or_else(|| 0..0),
+        Selection::Range(start, end) => {
+            let start = start
+                .and_then(|index| to_concrete_index(index, num_fields))
+                .unwrap_or(0);
+            let end = end
+                .and_then(|index| to_concrete_index(index, num_fields))
+                .unwrap_or_else(|| num_fields);
+            start..end
+        }
     }
 }
 
-fn to_concrete_index(selection: isize, num_fields: usize) -> usize {
-    if selection >= 0 {
-        selection as usize
+fn to_concrete_index(selection: isize, num_fields: usize) -> Option<usize> {
+    let num_fields = num_fields as isize;
+    if selection >= 0 && selection < num_fields {
+        Some(selection as usize)
+    } else if selection < 0 && num_fields + selection >= 0 {
+        Some((num_fields + selection) as usize)
     } else {
-        (num_fields as isize + selection).max(0) as usize
+        None
     }
 }
 
